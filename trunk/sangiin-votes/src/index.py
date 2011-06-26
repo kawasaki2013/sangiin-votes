a228fadd94f0f521ea3d3ab838a0fc412495c120
michinobumaeda@gmail.com <michinobumaeda@gmail.com@93727855-9774-130c-da9b-022aebefabc8>
2011-06-26 13:43:53 +0000

    for Test

--- trunk/sangiin-votes/src/index.py
+++ trunk/sangiin-votes/src/index.py
@@ -1,17 +1,32 @@
+'''
+sangiin-votes
+
+Copyright 2011 Michinobu Maeda.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+@author: Michinobu Maeda
+'''
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 
-
 class MainPage(webapp.RequestHandler):
-    
-    
+
     def get(self):
         self.response.headers['Content-Type'] = 'text/plain'
         self.response.out.write('Hello, webapp World!')
 
-
 application = webapp.WSGIApplication([('/', MainPage)], debug=True)
-
 
 def main():
     run_wsgi_app(application)
